92b8c7bf1cc19c3d7e2b4837d6570ee706350c5c
김경식 <envy7937@gmail.com>
2021-03-29 16:45:34 +0900

    Add Custom Input Component

---
+++ src/components/VInput.vue
@@ -0,0 +1,140 @@
+<!-- Input : text, password, email -->
+<template>
+  <div class="input-wrap">
+    <label :for="id" :style="customStyle">{{ label }}</label>
+    <input :type="type"
+           :id="id"
+           :name="name"
+           :value="value"
+           v-model="value"
+           :placeholder="placeholder"
+           :readonly="readonly"
+           :disabled="disabled"
+           :required="required"
+           :class="[(isErr === true) ? 'box-error' : '']"
+           @keyup="checkValue"/>
+    <div v-show="isMessage" :class="[(isErr === true) ? 'text-error' : '']" >{{ message }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "VInput",
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    backgroundColor: {
+      type: String,
+    },
+    inputClass: {
+      type: String,
+    },
+    rules: {
+      type: Array,
+    },
+  },
+  computed: {
+    customStyle: function () {
+      let custom = '';
+      if (this.color) custom += "color:" + this.color + ";";
+      if (this.backgroundColor) custom += "background-color:" + this.backgroundColor + ";";
+      return custom;
+    }
+  },
+  data: function () {
+    return {
+      isMessage: false,
+      message: [],
+      isErr: false,
+    }
+  },
+  methods: {
+    checkValue() {
+      if (this.required) {
+        if (!this.value) {
+          this.isMessage = true;
+          this.isErr = true;
+          this.message = "필수값입니다.";
+          return false;
+        } else {
+          this.isMessage = false;
+          this.message = '';
+        }
+      }
+
+      // 입력받은 rules에 맞춰서 유효성 검사
+      if (this.rules) {
+        let checkVal = this.value;
+        let isMsg = false;
+        let msg = [];
+
+        this.rules.forEach(function (value) {
+          let check = value(checkVal);
+          if (check !== true) {
+            isMsg = true;
+            msg.push(check);
+          }
+        });
+
+        this.isMessage = isMsg;
+        this.isErr = isMsg;
+        this.message = msg[0];
+      }
+
+      // input의 변경된 값을 부모 컴포넌트로 전달
+      this.$emit('change', this.value);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.input-wrap label {
+  margin-right: 5px;
+}
+
+.text-error {
+  color: red;
+}
+.box-error {
+  border: solid 1px red;
+}
+
+.text-primary {
+  color: #96caf5;
+}
+</style>
--- src/pages/create.vue
+++ src/pages/create.vue
@@ -2,8 +2,28 @@
   <div>
     <sub-layout>
       <div class="content">
-        <h1>Create Vue Project</h1>
-        <span>Vue Study 3주차 과제</span>
+        <h1>Custom Input Component</h1>
+        <span>Text, Email, Password</span>
+        <br/><br/>
+
+        <v-input id="email" name="email" type='email' label="E-mail" v-model="user.email" :rules=rules.email
+                 @change="setEmail" />
+        <br/>
+        <v-input id="password" name="password" type='password' label="Password" v-model="user.password" :rules=rules.password @change="setPassword"
+        />
+        <br/>
+        <v-input id="name" name="name" label="이름" v-model="user.name" :rules=rules.name @change="setName"
+        />
+        <br/>
+        <v-input id="phone" name="phone" label="휴대폰번호" v-model="user.phone" :rules=rules.phone @change="setPhone"
+        />
+        <br/>
+
+        <h1>Custom Selectbox Component</h1>
+        <br/><br/>
+
+        <h1>Custom TextArea Component</h1>
+        <br/><br/>
       </div>
     </sub-layout>
   </div>
@@ -11,10 +31,57 @@
 
 <script>
 import SubLayout from "@/components/layouts/sub/index"
+import VInput from "@/components/VInput"
 
 export default {
   name: "Create",
-  components: {SubLayout},
+  components: {SubLayout, VInput},
+  data: () => ({
+    user: {
+      name: '',
+      email: '',
+      password: '',
+      phone: '',
+    },
+    rules: {
+      email: [
+        value => !!value || '필수값입니다.',
+        value => (value).length >= 5 || '5자 이상으로 입력해주세요.',
+        value => (value).length <= 20 || '20자 이하로 입력해주세요.',
+        value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || '유효하지 않은 이메일입니다.'
+        },
+      ],
+      password: [
+        value => !!value || '필수값입니다.',
+        value => (value).length >= 8 || '8자 이상으로 입력해주세요.',
+        value => (value).length <= 20 || '20자 이하로 입력해주세요.',
+      ],
+      name: [
+        value => !!value || '필수값입니다.',
+        value => (value).length >= 2 || '2자 이상으로 입력해주세요.',
+        value => (value).length <= 10 || '10자 이하로 입력해주세요.',
+      ],
+      phone: [
+        value => !!value || '필수값입니다.',
+      ]
+    }
+  }),
+  methods: {
+    setEmail(val) {
+      this.user.email = val;
+    },
+    setPassword(val) {
+      this.user.password = val;
+    },
+    setName(val) {
+      this.user.name = val;
+    },
+    setPhone(val) {
+      this.user.phone = val;
+    },
+  }
 }
 </script>
 
