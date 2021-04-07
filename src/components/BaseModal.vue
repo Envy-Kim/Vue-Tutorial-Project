b1aed13640ec0094657762829543faeb8cba1773
김경식 <envy7937@gmail.com>
2021-04-07 15:12:32 +0900

    Add : 모달과 리스트에 애니메이션 추가

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,18 +1,24 @@
 <template>
   <transition v-if="show" name="modal-fade">
-    <div class="modal-wrap">
-      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content">
-          <div class="modal-header">
-            <slot name="modal-title"/>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="$emit('close')">&times;</button>
-          </div>
-          <div class="modal-body">
-            <slot/>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="$emit('close')">닫기</button>
-            <!-- <button type="button" class="btn btn-primary disabled">Understood</button> -->
+    <!--
+    모달 외부를 누르면 닫히도록 모달외부에 해당하는 부분에 @mousedown 을 이용하여 $emit('close') 해준다.
+    @click 대신 @mousedown을 사용한 이유는 click의 경우 눌렀다가 뗀 위치가 modal-mask일 경우에도 동작하기 때문
+    -->
+    <div class="modal-mask" @mousedown="$emit('close')">
+      <div class="modal-wrap">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" @mousedown.stop>
+          <div class="modal-content">
+            <div class="modal-header">
+              <slot name="modal-title"/>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="$emit('close')">&times;</button>
+            </div>
+            <div class="modal-body">
+              <slot/>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="$emit('close')">닫기</button>
+              <!-- <button type="button" class="btn btn-primary disabled">Understood</button> -->
+            </div>
           </div>
         </div>
       </div>
@@ -33,8 +39,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.modal-wrap {
+.modal-mask {
   position: fixed;
   background-color: rgb(0, 0, 0);
   background-color: rgba(0, 0, 0, 0.4);
@@ -55,5 +60,13 @@ export default {
   border: none;
   font-size: 30px;
   background-color: transparent;
+}
+
+.modal-fade-enter-active, .modal-fade-leave-active {
+  transition: opacity .5s;
+}
+
+.modal-fade-enter, .modal-fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/board/Index.vue
+++ src/components/board/Index.vue
@@ -3,12 +3,14 @@
     <keep-alive>
       <!-- :is 를 이용한 동적 컴포넌트 -->
       <div v-if="mode === 1">
-        <component :is="viewComponent"
-                   :columns="columns"
-                   :items="items"
-                   v-model="$attrs.value"
-                   @input="$emit('input', $attrs.value)"
-                   @click="$emit('click')" ></component>
+        <transition name="list-fade" mode="out-in" appear>
+          <component :is="viewComponent"
+                     :columns="columns"
+                     :items="items"
+                     v-model="$attrs.value"
+                     @input="$emit('input', $attrs.value)"
+                     @click="$emit('click')"></component>
+        </transition>
         <!--
         $attrs
         props로 인식(및 추출)되지 않는 부모 범위 속성 바인딩.
@@ -19,27 +21,29 @@
 
       <!-- v-if/else 를 이용한 컴포넌트 변경 -->
       <div v-else>
-        <vue-table v-if="bbsType === 'list'"
-                   :columns="columns"
-                   :items="items"
-                   v-model="selectItem"
-                   @input="$emit('input', selectItem)"
-                   @click="$emit('click')" >
-        </vue-table>
-
-        <vue-gallery v-else-if="bbsType === 'gallery'"
+        <transition name="list-fade" mode="out-in" appear>
+          <vue-table v-if="bbsType === 'list'"
+                     :columns="columns"
                      :items="items"
                      v-model="selectItem"
                      @input="$emit('input', selectItem)"
                      @click="$emit('click')" >
-        </vue-gallery>
+          </vue-table>
 
-        <vue-webzine v-else
-                     :items="items"
-                     v-model="selectItem"
-                     @input="$emit('input', selectItem)"
-                     @click="$emit('click')" >
-        </vue-webzine>
+          <vue-gallery v-else-if="bbsType === 'gallery'"
+                       :items="items"
+                       v-model="selectItem"
+                       @input="$emit('input', selectItem)"
+                       @click="$emit('click')" >
+          </vue-gallery>
+
+          <vue-webzine v-else
+                       :items="items"
+                       v-model="selectItem"
+                       @input="$emit('input', selectItem)"
+                       @click="$emit('click')" >
+          </vue-webzine>
+        </transition>
       </div>
     </keep-alive>
   </div>
@@ -101,5 +105,17 @@ export default {
 </script>
 
 <style scoped>
+.list-fade-enter-active, .list-fade-leave-active {
+  transition: all .3s;
+}
 
+.list-fade-enter {
+  opacity: 0;
+  transform: translateX(50px);
+}
+
+.list-fade-leave-to {
+  opacity: 0;
+  transform: translateX(-50px);
+}
 </style>
